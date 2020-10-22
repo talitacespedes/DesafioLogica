300721eabd0c93b7d3405006079aad6b49de2352
Talita <talita.cespedes@leadr.com.br>
2020-10-22 17:08:18 -0300

    adding consolidated values ​​table

--- src/utils.js
+++ src/utils.js
@@ -3,6 +3,29 @@ function FormatPrice(number, minDigits = 2) {
     return number.replace('.', ',').replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 };
 
+function getTotal(total, item) {
+    return total + item.value;
+};
+
+function getMonth(key) {
+    let months = {
+        01 : 'Janeiro',
+        02 : 'Fevereiro',
+        03 : 'Março',
+        04 : 'Abril',
+        05 : 'Maio',
+        06 : 'Junho',
+        07 : 'Julho',
+        08 : 'Agosto',
+        09 : 'Setembro',
+        10 : 'Outubro',
+        11 : 'Novembro',
+        12 : 'Dezembro'
+    };
+
+    return months[key];
+};
+
 // table fatura
 Object.values(list).map(item => {
 
@@ -18,4 +41,19 @@ Object.values(list).map(item => {
         let data = document.createElement("td");
         tbody.appendChild(data).innerText = value;
     })
+});
+
+// table consolidado
+Object.entries(group).map(([key, value]) => {
+
+    let row = document.createElement("tr");
+    let tbody = document.getElementById("tbody_consolidado").appendChild(row);
+
+    let dataMonth = document.createElement("td");
+    tbody.appendChild(dataMonth).innerText = getMonth(key);
+
+    let total = value.reduce(getTotal, 0);
+
+    let dataTotal = document.createElement("td");
+    row.appendChild(dataTotal).innerText = `R$ ${FormatPrice(total)}`;
 });
